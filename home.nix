8e9e0d07f4fa088dad772c4fa8ba8f6b48a7a2dc
diana-anna <17442895+diana-anna@users.noreply.github.com>
2025-08-31 11:48:10 -0500

    added home.org

--- home.nix
+++ home.nix
@@ -1,93 +1,47 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "diana";
   home.homeDirectory = "/home/diana";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = with pkgs; [
-    haskellPackages.nixfmt
-    htop
-    ncdu
-    pandoc
-    pls
-    signal-desktop
-    telegram-desktop
-    tmux
-    tree
-    vim
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    (nerdfonts.override { fonts = [ "UbuntuMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
 
   home.sessionPath =
     [ "$HOME/.nix-profile/bin" "$HOME/.nix-profile/share/applications" ];
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/diana/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = { EDITOR = "emacs"; };
+  home.packages = with pkgs; [
+    haskellPackages.nixfmt
+    htop
+    ncdu
+    nerd-fonts.ubuntu-mono
+    pandoc
+    pls
+    signal-desktop
+    telegram-desktop
+    texlab # LaTeX LSP
+    tmux
+    tree
+    vim
+
+    (writeShellScriptBin "hm-history" ''
+      nix profile diff-closures --profile ~/.local/state/nix/profiles/home-manager
+    '')
+    (writeShellScriptBin "hm-news" ''
+      home-manager news --flake ~/dotfiles
+    '')
+    (writeShellScriptBin "hm-switch" ''
+      home-manager switch --flake ~/dotfiles
+    '')
+  ];
+
+  programs.home-manager.enable = true;
+
+  programs.emacs.enable = true;
+
+  programs.starship.enable = true;
 
   nix = {
     package = pkgs.nix;
     settings.experimental-features = [ "nix-command" "flakes" ];
   };
-
-  programs.emacs.enable = true;
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-  programs.starship.enable = true;
-
 }
