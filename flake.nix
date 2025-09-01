0f42af9c98788fa2e1a1685f2f28fc3be9508668
diana-anna <17442895+diana-anna@users.noreply.github.com>
2025-09-01 15:52:13 -0500

    add flake for home manager

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Diana's Home Manager Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, home-manager }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations."diana" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
