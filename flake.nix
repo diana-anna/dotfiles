d45ca0e255fcf0875c5ed02ed0706d791079634c
diana-anna <17442895+diana-anna@users.noreply.github.com>
2026-01-01 15:10:41 -0600

    added flake-utils and tried making flake work for linux and macos

--- flake.nix
+++ flake.nix
@@ -3,24 +3,25 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager }:
-    let
-      system = "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      homeConfigurations."diana" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+  outputs = inputs@{ self, nixpkgs, flake-utils, home-manager }:
+    flake-utils.lib.eachSystem [ "aarch64-darwin" "x86_64-linux" ] (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.homeConfigurations."diana" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
 
-        modules = [ ./home.nix ];
+          modules = [ ./home.nix ];
 
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-      };
-    };
+          # Optionally use extraSpecialArgs
+          # to pass through arguments to home.nix
+        };
+      });
 }
